382171ffb0f24401e55d5ae40793de0e20eedf79
sidkadge <siddheshkadgemitech@gmail.com>
2025-05-15 12:30:38 +0530

    new changes with crud

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,15 +7,20 @@ use CodeIgniter\Router\RouteCollection;
  */
 
 // $routes->get('/', 'Home::index');
-$routes->get('/', 'Blog::index');
-$routes->get('blog', 'Blog::index');
-$routes->get('blog/(:segment)', 'Blog::view/$1');
+$routes->get('/', 'BlogController::index');
+$routes->get('blog', 'BlogController::index');
+$routes->get('blog/(:segment)', 'BlogController::view/$1');
 $routes->get('dashboard', 'BlogController::getdashboard');
 $routes->post('addblogs', 'BlogController::addblog');
-$routes->get('getaddblog', 'BlogController::getaddblog');
 $routes->post('authenticate', 'Home::authenticate');
 $routes->get('logout', 'Home::logout');
+$routes->get('getbloglist', 'BlogController::getbloglist');
+$routes->get('getaddblog', 'BlogController::getaddblog');
 
 
-$routes->get('login', 'Blog::login');
+$routes->post('updateblog/(:num)', 'BlogController::updateblog/$1');
+$routes->post('deleteblog/(:num)', 'BlogController::deleteblog/$1');
+
+
+$routes->get('login', 'BlogController::login');
 
