d27c6baeb676107885f4f171f3ce3bd7ae2062b6
sidkadge <siddheshkadgemitech@gmail.com>
2025-05-15 12:35:25 +0530

    blog controller file

--- app/Controllers/BlogController.php
+++ app/Controllers/BlogController.php
@@ -17,7 +17,7 @@ class BlogController extends BaseController
         return view('blog/index', $data);
     }
 
-    public function view($slug)
+     public function view($slug)
     {
         $model = new BlogModel();
         $blog = $model->where('slug', $slug)->first();
@@ -31,6 +31,22 @@ class BlogController extends BaseController
 
         return view('blog/view', $data);
     }
+
+    public function ajaxView($slug)
+    {
+        $model = new BlogModel();
+        $blog = $model->where('slug', $slug)->first();
+
+        if (!$blog) {
+            throw new \CodeIgniter\Exceptions\PageNotFoundException('Blog not found');
+        }
+
+        $data['blog'] = $blog;
+        $data['other_blogs'] = $model->where('slug !=', $slug)->findAll();
+
+        return view('blog/ajax_view', $data);
+    }
+
     public function login()
     {
      return view('login');
